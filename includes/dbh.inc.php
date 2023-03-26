2861b8dec96e43ab441575a5f9b1412d615a71b2
KaiEn-dev <128709270+KaiEn-dev@users.noreply.github.com>
2023-03-27 01:52:19 +0800

    Add files via upload

---
+++ includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "personality_assessment";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
